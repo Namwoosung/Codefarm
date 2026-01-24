dfc07e9b714eeb1b16bd0ad2d7106266c6b03c35
Yeon <gomsi0724@naver.com>
2026-01-25 01:51:52 +0900

    feat(frontend): setup initial environment

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,38 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: () => import('@/views/MainView.vue')
+    },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: () => import('@/views/ProfileView.vue')
+    },
+    {
+      path: '/problem/:id',
+      name: 'problem',
+      component: () => import('@/views/ProblemView.vue')
+    },
+    {
+      path: '/roadmap',
+      name: 'roadmap',
+      component: () => import('@/views/RoadmapView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('@/views/SignupView.vue')
+    },
+  ],
 })
 
 export default router
---
+++ frontend/src/views/RoadmapView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Roadmap View</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
