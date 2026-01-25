b8a6c7f43b2f92bb3e6a5ee98d10882deea5a196
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2026-01-25 22:24:54 +0900

    feat(backend): add base response format

---
+++ backend/src/main/java/com/ssafy/codefarm/common/dto/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.ssafy.codefarm.common.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final String errorCode;
+    private String message;
+    private Object data;
+
+    public ErrorResponse(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorResponse(String errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public ErrorResponse(String errorCode, String message, Object data) {
+        this.errorCode = errorCode;
+        this.message = message;
+        this.data = data;
+    }
+
+
+}
