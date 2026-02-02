4cf472bcbd67308bf55e76c616fb91442a9297d9
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2026-02-02 10:56:16 +0900

    feat(backend): add referesh token redis service

---
+++ backend/src/main/java/com/ssafy/codefarm/user/service/RefreshTokenRedisService.java
@@ -0,0 +1,68 @@
+package com.ssafy.codefarm.user.service;
+
+import com.ssafy.codefarm.common.exception.CustomException;
+import com.ssafy.codefarm.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenRedisService {
+    private final StringRedisTemplate redisTemplate;
+
+    private static final String PREFIX = "app:auth:refresh:";
+
+    private String buildKey(Long userId) {
+        return PREFIX + userId;
+    }
+
+    /**
+     * Refresh Token 저장 (로그인 or 재발급 시)
+     */
+    public void save(Long userId, String refreshToken, Duration ttl) {
+
+        String key = buildKey(userId);
+
+        redisTemplate.opsForValue().set(key, refreshToken, ttl);
+    }
+
+    /**
+     * Refresh Token 조회
+     */
+    public String get(Long userId) {
+
+        return redisTemplate.opsForValue().get(buildKey(userId));
+    }
+
+    /**
+     * Refresh Token 검증 (RTR 로직)
+     */
+    public void validate(Long userId, String refreshToken) {
+
+        String savedToken = get(userId);
+
+        if (savedToken == null) {
+            throw new CustomException(
+                    "로그인 정보가 만료되었습니다.",
+                    ErrorCode.UNAUTHORIZED
+            );
+        }
+
+        if (!savedToken.equals(refreshToken)) {
+            throw new CustomException(
+                    "유효하지 않은 Refresh Token입니다.",
+                    ErrorCode.INVALID_TOKEN
+            );
+        }
+    }
+
+    /**
+     * 로그아웃 시 삭제
+     */
+    public void delete(Long userId) {
+        redisTemplate.delete(buildKey(userId));
+    }
+}
